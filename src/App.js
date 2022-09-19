64f27186489a9e24922f27843584c1c264e8d98e
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 12:00:18 -0400

    configurate logged-in-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { LoggedOutRouter } from 'routers/logged-out-router';
 import { LoggedInRouter } from 'routers/logged-in-router';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
 
-	const isLogin = false;
+	const isLogin = useSelector(state => state.userReducer.isLogin);
 	
 	return isLogin ? <LoggedInRouter /> : <LoggedOutRouter />;
 };
--- src/routers/logged-in-router.js
+++ src/routers/logged-in-router.js
@@ -1,9 +1,26 @@
 import { Fragment } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+const clientRoutes = [
+	
+	{
+		path: '*',
+		component: <Navigate to="/" replace />
+	}
+];
 
 export const LoggedInRouter = () => {
 	return (
 		<Fragment>
-			router in
+			<Nav />
+			<main>
+				<Routes>
+					{clientRoutes.map(route => (
+						<Route path={route.path} exact key={route.path} element={route.component} />
+					))}
+				</Routes>
+			</main>
 		</Fragment>
 	);
+
 };
