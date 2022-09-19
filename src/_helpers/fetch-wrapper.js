a0dbb958de21048cdc47cc2b72f25bcc9c0d014a
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 11:45:33 -0400

    create fetch-wrapper helpers

---
+++ src/_helpers/fetch-wrapper.js
@@ -0,0 +1,61 @@
+import { TOKEN_ENV } from "constants";
+import { baseUrl } from "constants";
+import JwtService from "services/jwtService";
+
+export const fetchWrapper = {
+    get: request('GET'),
+    post: request('POST'),
+    put: request('PUT'),
+    delete: request('DELETE'),
+    patch: request('PATCH')
+};
+
+function request(method) {
+    return (url, body) => {
+
+        const requestOptions = {
+            method,
+            headers: authHeader(url)
+        };
+        if (body) {
+            requestOptions.headers['Content-Type'] = 'application/json';
+            requestOptions.body = JSON.stringify(body);
+        }
+        return fetch(url, requestOptions).then(handleResponse);
+    }
+}
+
+function authHeader(url) {
+    
+    const token = authToken();
+    const isLoggedIn = !!token;
+    const isApiUrl = url.startsWith(baseUrl);
+    if (isLoggedIn && isApiUrl) {
+        return { Authorization: `Bearer ${token}` };
+    } else {
+        return {};
+    }
+}
+
+function authToken() {
+    return localStorage.getItem(TOKEN_ENV);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+
+        if (!response.ok) {
+            
+            if ([401, 403].includes(response.status)) {                
+                JwtService.setSession(null);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }       
+        
+        
+        return data;
+    });
+}
