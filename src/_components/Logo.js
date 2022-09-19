d8ad529dbc85b615a8708c5101f2ff2ef6c6d4e8
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 11:40:58 -0400

    create component logo

---
+++ src/_components/Logo.js
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material";
+
+const Logo = () => {
+
+    return (
+		<div className="flex">
+		<Typography sx={{ color: 'gray' }} component="span" variant="h6">
+			insta
+		</Typography>
+		<Typography sx={{ color: 'teal', fontWeight: '800' }} component="span" variant="h5">
+			SHARE
+		</Typography>
+	</div>
+	);
+	
+};
+
+export default Logo;
