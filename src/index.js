bff8a09906b6342d6aa099f57062058c760ea90d
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 11:27:55 -0400

    create react-redux provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
+import { store } from './_store';
 import { App } from './App';
+import 'styles/styles.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 );
