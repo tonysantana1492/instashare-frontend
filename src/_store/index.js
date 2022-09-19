7cfcd582dfc5c0d03d2f1cefc74d99b409049b97
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-19 11:50:34 -0400

    import reducers into configureStore

--- src/_store/index.js
+++ src/_store/index.js
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { loginReducer } from './login.slice';
+import { registerReducer } from './register.slice';
+import { userReducer } from './users.slice';
+
+export * from './login.slice';
+export * from './users.slice';
+export * from './register.slice';
 
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		loginReducer: loginReducer,
+		userReducer: userReducer,
+		registerReducer: registerReducer
+	}
 });
