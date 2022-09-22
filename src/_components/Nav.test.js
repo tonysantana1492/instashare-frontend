daad15b7b2668b79d00581d9b95a0468ae67a667
Tony Santana Lopez <tonysantana1492@gmail.com>
2022-09-22 18:56:40 -0400

    unit test for Nav component

---
+++ src/_components/Nav.test.js
@@ -0,0 +1,20 @@
+
+import { waitFor } from '@testing-library/react';
+import { renderWithProviders, setupStore } from '_helpers/test-utils';
+import { setUser } from '_store';
+import Nav from './Nav';
+
+describe('<Nav />', () => {
+	it('renders Nav', async () => {
+		const store = setupStore();
+		const payload = { token: 'dfdfdf', user: { username: 'dsdsds', displayname: 'dssds' } };
+        const { getByText, debug } = renderWithProviders(<Nav />, { store });
+        
+		await waitFor(() => {
+            
+			store.dispatch(setUser(payload));			
+		});
+        
+		getByText('SHARE');
+	});
+});
